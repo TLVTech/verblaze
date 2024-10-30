b1538b067a6c3d28bb2f8058819381a776fc27e3
shmooel28 <93682110+shmooel28@users.noreply.github.com>
2024-10-30 10:50:21 +0200

    serverless

--- run-gradio.sh
+++ run-gradio.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+LOG_DIR="/home/ubuntu/verblaze"
 
-python3 -m videollama2.serve.controller --host 0.0.0.0 --port 10000 > /tmp/videollama2.log 2>&1 &
-python3 -m videollama2.serve.gradio_web_server --controller http://localhost:10000 --model-list-mode reload > /tmp/gradio.log 2>&1 &&
+python3 -m videollama2.serve.controller --host 0.0.0.0 --port 10000 > $LOG_DIR/videollama2.log 2>&1 &
+python3 -m videollama2.serve.gradio_web_server --controller http://localhost:10000 --model-list-mode reload > $LOG_DIR/script_output.log 2>&1 &
